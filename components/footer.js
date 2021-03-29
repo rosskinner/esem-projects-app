eb4bcee84a7ccef6a249ec81fe2274c80aa4e612
Rosalind Skinner <ros.skinner@gmail.com>
2021-03-29 12:12:24 +1100

    show socials only if there are any

--- components/footer.js
+++ components/footer.js
@@ -9,17 +9,19 @@ const Footer = ({ global, pageProps }) => {
       <div className='w-50'>
         © {global.siteName} 2021
       </div>
-      <div className='w-50 tr'>
-        {global.social.facebook &&
-          <a className='f6 details underline secondary-color ph3' href={global.social.facebook} rel='noreferrer' target='_blank'>Facebook</a>}
-        {global.social.instagram &&
-          <a className='f6 details underline secondary-color ph3' href={global.social.instagram} rel='noreferrer' target='_blank'>Instagram</a>}
-        {global.social.twitter &&
-          <a className='f6 details underline secondary-color ph3' href={global.social.twitter} rel='noreferrer' target='_blank'>Twitter</a>}
-        {global.social.linkedin &&
-          <a className='f6 details underline secondary-color ph3' href={global.social.linkedin} rel='noreferrer' target='_blank'>LinkedIn</a>}
+      {global.social &&
+        <div className='w-50 tr'>
+          {global.social.facebook &&
+            <a className='f6 details underline secondary-color ph3' href={global.social.facebook} rel='noreferrer' target='_blank'>Facebook</a>}
+          {global.social.instagram &&
+            <a className='f6 details underline secondary-color ph3' href={global.social.instagram} rel='noreferrer' target='_blank'>Instagram</a>}
+          {global.social.twitter &&
+            <a className='f6 details underline secondary-color ph3' href={global.social.twitter} rel='noreferrer' target='_blank'>Twitter</a>}
+          {global.social.linkedin &&
+            <a className='f6 details underline secondary-color ph3' href={global.social.linkedin} rel='noreferrer' target='_blank'>LinkedIn</a>}
 
-      </div>
+        </div>}
+
     </footer>
   )
 }
