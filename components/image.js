1fc4310b0d50f89d48be2591ba9cec45cf824bb9
Rosalind Skinner <ros.skinner@gmail.com>
2021-05-03 11:22:50 +1000

    media query for grid column

--- components/image.js
+++ components/image.js
@@ -3,13 +3,9 @@ import { getStrapiMedia } from '../lib/api'
 import { m, useAnimation, LazyMotion, domAnimation } from 'framer-motion'
 import Image from 'next/image'
 
-
-
-
 const Img = ({ media }) => {
-  const [totalHeight, setTotalHeight] = useState(0)
-  let width = 'w-100'
-  let cols = 1
+  const width = 'w-100'
+  const cols = 1
 
   let gridStyle = 'onecol'
 
@@ -23,67 +19,30 @@ const Img = ({ media }) => {
     visible: { opacity: 1 },
     hidden: { opacity: 0 }
   }
-  
-  
+
   const handleResize = () => {
-    let tHeight = 0
+    // let tHeight = 0
     const widthEm = window.innerWidth / parseFloat(
       getComputedStyle(
         document.querySelector('body')
       )['font-size']
     )
-        
-    if (widthEm < 60) {
-      setTotalHeight(0)
 
-    } else {
+    if (widthEm > 60) {
       media.map((m, key) => {
         m.index = key
         m.orientation = 'landscape'
-  
+
         let ratio = 0.625
         if (m.width < m.height) {
           m.orientation = 'portrait'
           ratio = 1.25
-  
-          
         }
-        
-        const width = ((window.innerWidth * 0.75) / cols) - ((2*16)*3)
-        m.newHeight = width*ratio + (2*16)*2
-        tHeight += m.newHeight 
-      })
-  
-      let checkHeight = 0
-      let halfHeight = tHeight/2
-      let remainder = 0
-      let setHeight = 0
-      let found = false
-  
-      media.map((m, key) => {
-        checkHeight += m.newHeight
-        if(!found) {
-          if (checkHeight > halfHeight) {
-            found = true
-            remainder = checkHeight - halfHeight
-          }
-          if(remainder > m.newHeight/2) {
-            setHeight = halfHeight + m.newHeight/2
-          } else {
-            setHeight = checkHeight
-          }
-        }
-      })
-      if (media.length === 1) {
-        setHeight = media[0].newHeight
-      }
-      console.log(setHeight)
-        setTotalHeight(setHeight)
-    }
-    
-    
 
-    
+        const width = ((window.innerWidth * 0.75) / cols) - ((2 * 16) * 3)
+        m.newHeight = width * ratio + (2 * 16) * 2
+      })
+    }
   }
   useEffect(() => {
     window.addEventListener('resize', handleResize, false)
@@ -91,11 +50,9 @@ const Img = ({ media }) => {
     return () => window.removeEventListener('resize', handleResize)
   })
 
-  const style = totalHeight === 0 ? {height:'auto'} : {maxHeight :`${totalHeight}px`}
-
   return (
-    <div className={`w-100`} >
-      
+    <div className='w-100'>
+
       <div className={`w-100 grid ${gridStyle}`}>
         {media.map((me, key) => {
           const [loaded, setLoaded] = useState(false)
@@ -104,43 +61,42 @@ const Img = ({ media }) => {
           const url = (me.formats === null || Object.keys(me.formats).length === 0) ? me : (me.formats.large ? me.formats.large : me)
           const mediaUrl = getStrapiMedia(url)
           let ratio = '8x5'
-          
-          if (me.width < me.height) ratio = '8x10'
-          
-          useEffect(() => {
-            if(loaded){
 
+          if (me.width < me.height) ratio = '8x10'
+
+          useEffect(() => {
+            if (loaded) {
               animationControls.start('visible')
             }
-          },[loaded])
-          
+          }, [loaded])
+
           const checkLoaded = (e) => {
             setLoaded(true)
           }
-          
-          return (
 
+          return (
 
             <LazyMotion key={key} features={domAnimation}>
 
-
-              <m.div key={key} className={`relative ${width} grid-item grid-item-${me.orientation}`}
-              initial='initial'
-              animate={animationControls}
-              variants={animationVariants}
-              transition={{ ease: "easeIn", duration: 1 }}>
-                <div  className={`aspect-ratio aspect-ratio--${ratio}`}>
-                  <Image className='background-image cover center aspect-ratio--object' src={mediaUrl} layout='fill' objectFit='cover'
+              <m.div
+                key={key} className={`relative ${width} grid-item grid-item-${me.orientation}`}
+                initial='initial'
+                animate={animationControls}
+                variants={animationVariants}
+                transition={{ ease: 'easeIn', duration: 1 }}
+              >
+                <div className={`aspect-ratio aspect-ratio--${ratio}`}>
+                  <Image
+                    className='background-image cover center aspect-ratio--object' src={mediaUrl} layout='fill' objectFit='cover'
                     alt={me.alternativeText}
                     title={me.caption}
                     quality={100}
                     onLoad={checkLoaded}
-            />
+                  />
                 </div>
-                
-                  <div className='f8 caption pt2 pl2 pl0-l'><span>{me.caption}</span></div>
-                
-                
+
+                <div className='f8 caption pt2 pl2 pl0-l'><span>{me.caption}</span></div>
+
               </m.div>
             </LazyMotion>
 
