90c2fe3e596b79d6ff58cc6f1533adca2229db65
Rosalind Skinner <ros.skinner@gmail.com>
2021-05-24 09:09:44 +1000

    change priority sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,24 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://esemprojects.com',
   generateRobotsTxt: true,
-  exclude: ['/project/*', '/article/*']
+  priority: 0.5,
+  transform: async (config, path) => {
+    // custom function to ignore the path
+    if (path === '/projects' || path === '/about' || path === '/articles' || path === '/') {
+      return {
+        loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+        changefreq: config.changefreq,
+        priority: 1,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined
+      }
+    }
+
+    // Use default transformation for all other cases
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined
+    }
+  }
 }
