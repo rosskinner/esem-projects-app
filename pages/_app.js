ddd7ade62228f87360b3599792575b3fd5fc8bb7
Rosalind Skinner <ros.skinner@gmail.com>
2023-06-18 14:49:48 +1000

    google tag manager script

--- pages/_app.js
+++ pages/_app.js
@@ -93,18 +93,17 @@ const EsemApp = ({ Component, pageProps, router }) => {
         <link href={getStrapiMedia(global.favicon)} rel='apple-touch-icon' />
 
       </Head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-HDWHVM1K5L" strategy="afterInteractive"></Script>
-      <Script strategy="afterInteractive">
+      
+      <Script strategy="beforeInteractive">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-  
-          gtag('config', 'G-HDWHVM1K5L');
-        `
-        }
-        
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TQPT3B8');
+        `}
       </Script>
+
       <div className='white'>
         <Nav {...pageProps} scroll={scroll} />
         <GlobalContext.Provider value={global}>
