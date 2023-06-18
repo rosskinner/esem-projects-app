eb2aa38fbd27d3e3dcf6729d41300bdd5a6ceb53
Rosalind Skinner <ros.skinner@gmail.com>
2023-06-18 14:53:45 +1000

    add iframe

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,10 @@ class Layout extends Document {
 
         <Head />
         
-        
+
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQPT3B8"
+      height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe></noscript>
+
         <body>
           <Main />
           <NextScript />
